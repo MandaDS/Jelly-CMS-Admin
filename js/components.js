4c9b26774e95e57e3260c31c34b2e1ebae145748
Amanda Eldreth <amanda@efellemedia.com>
2015-06-22 13:03:53 -0700

    Revert "Move modernizr to components file"
    This reverts commit db4703ec06068ead2d82f5ba3f0ce86ec22fbabb.

--- js/components.js
+++ js/components.js
@@ -1,81 +1,41 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.min.css">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="css/main.css">
-		<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic|Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic' rel='stylesheet' type='text/css'>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/modernizr.custom.js"></script>
-	</head>
-	<body>
-	<header>
-		<div class="header-left">
-			<ul>
-				<li class="logo"><a href="#"><img src="css/img/fusion-logo.png"></a></li>
-				<li class="nav-btn">
-					<div class="cbutton cbutton--effect-tamara">
-						<a href="#"><i class="fa fa-bars" id="menu-button"></i></a>
-					</div>
-				</li>
-			</ul>
-		</div>
-		<div class="header-right"></div>
-	</header>
+// Enable full responsiveness
 
-	<div id="side-menu">
-		<nav>
-			<ul class="main-menu">
-				<li><a href="#"><i class="fa fa-home"></i>Dashboard</a></li>
-				<li><a href="#"><i class="fa fa-newspaper-o"></i>Blog</a></li>
-				<li><a href="#"><i class="fa fa-file-text-o"></i>Pages</a></li>
-				<li><a href="#"><i class="fa fa-paint-brush"></i>Appearance</a></li>
-				<li><a href="#"><i class="fa fa-cubes"></i>Workshop</a></li>
-				<li><a href="#"><i class="fa fa-users"></i>Users</a></li>
-			</ul>
-		</nav>
-	</div>
+// Copyright 2014-2015 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
 
-	<div id="page" class="content">
-		<div id="styles" class="container">
-			<h1>Example Styles</h1>
-			<section>
-				<h2>Primary</h2>
-				<div class="style-primary"> .primary </div>
-				<div class="style-primary-dark">.primary-dark</div>
-				<div class="style-primary-light">.primary-light</div>
-				<div class="style-primary-bright">.primary-bright</div>
-			</section>
-			
-			<section>
-				<h2>Accent</h2>
-				<div class="style-accent">.accent</div>
-				<div class="style-accent-dark">.accent-dark</div>
-				<div class="style-accent-light">.accent-light</div>
-				<div class="style-accent-bright">.accent-bright</div>
-			</section>
-			
-			<section>
-				<h2>Menu</h2>
-				<div class="style-menu">.menu</div>
-				<div class="style-menu-dark">.menu-dark</div>
-				<div class="style-menu-light">.menu-light</div>
-				<div class="style-menu-bright">.meun-bright</div>
-			</section>
-			
-			<section>
-				<h2>Background</h2>
-				<div class="style-background">.background</div>
-				<div class="style-background-dark">.background-dark</div>
-				<div class="style-background-light">.background-light</div>
-				<div class="style-background-bright">.background-bright</div>
-			</section>
-		</div>
-	</div>
-	<script src="js/classie.js"></script>
-	<script src="js/components.js"></script>
-	</body>
-</html>
+// Toggle Menu
+$(document).ready(function() {
+	var menuLeft = document.getElementById( 'side-menu' ),
+		showLeft = document.getElementById( 'menu-button' ),
+		body = document.body;
+	showLeft.onclick = function() {
+		classie.toggle( this, 'active' );
+		classie.toggle( this, 'fa-bars');
+		classie.toggle( this, 'fa-close');
+		classie.toggle( body, 'push' );
+		classie.toggle( menuLeft, 'open' );
+		disableOther( 'showLeft' );
+	};
+	function disableOther( button ) {
+		if( button !== 'showLeft' ) {
+			classie.toggle( showLeft, 'disabled' );
+		}
+	}
+	// Add/Remove classes if click outside of menu
+	$("#page").click(function(){
+		  $(showLeft).removeClass("active");
+		  $(showLeft).removeClass("fa-close");
+		  $(showLeft).addClass("fa-bars");
+	      $(menuLeft).removeClass("open");
+	      $(body).removeClass("push");
+	});
+});
